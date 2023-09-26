9b75cb9d9df165d7decd22627503cf3f2a910d7a
Vitaly <vitaly.petrov.com@gmail.com>
2023-09-26 12:51:36 +0300

    Урок 2.19. Страница товара

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,10 +2,27 @@
 
 namespace app\controllers;
 
-class ProductController extends AppController { // Контроллер (класс) для обработки продуктов
+use app\models\Product;
+use wfm\App;
 
-    public function viewAction () { // Метод для отображения страницы продукта
+/** @property Product $model  */ // Подключаем модель для удобства работы с ProductController (редактор будет делать нам подсказки)
 
+class ProductController extends AppController { // Контроллер (класс) для обработки товаров
+
+    public function viewAction () { // Метод для отображения страницы товара
+        $lang = App::$app->getProperty ('language');  // Получение языка в переменную $lang
+        $product = $this->model->get_product ($this->route['slug'], $lang); // Получение данных о продукте в переменную $product
+        /*debug ($product,1); // Проверка правильности выполнения запроса*/
+
+        if (!$product) { // Проверка, достали ли мы нужный продукт (если, например, пользователь пришел к нам на сайт по битой ссылке)
+            throw new \Exception("Товар по запросу {$this->route['slug']} не найден!", 404);  // Если нет, показываем сообщение об ошибке с кодом 404 (если отключен DEBUG)
+        }
+        /*debug ($product); // Проверка правильности выполнения запроса*/
+
+        $gallery = $this->model->get_gallery ($product['id']);  // Получаем все изображения из галереи для конкретного товара
+        /*debug ($gallery); // Проверка правильности выполнения запроса*/
+        $this->setMeta ($product['title'], $product['description'], $product['keywords']);  // Передадим все недостающие данные
+        $this->set (compact ('product', 'gallery'));  // Передаем сами данные используя метод "set"
 
 
     }
---
+++ app/models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+
+use RedBeanPHP\R;
+
+class Product extends AppModel { // Модель для работы со страницами товара
+
+    public function get_product ($slug, $lang):array { // Метод, с помощью которого нам нужно объединить таблицы product и product_description, чтобы получить нужные данные по входным данным $slug и $lang
+        return R::getRow ("SELECT p.*, pd.* FROM product p JOIN product_description pd on p.id = pd.product_id WHERE p.status = 1 AND p.slug = ? AND pd.language_id = ?", [$slug, $lang['id']]); // Метод для получения данных товара из БД
+
+    }
+
+    public function get_gallery ($product_id):array {  // Метод, который будет получать изображения галереи для конкретного товара
+        return R::getAll ("SELECT * FROM product_gallery WHERE product_id = ?", [$product_id]); // Метод для получения всех изображений галереи для конкретного товара
+    }
+
+}
