e548d636f9fd30f0a10728170a53c8bbe08d9a35
Vitaly <vitaly.petrov.com@gmail.com>
2023-10-20 17:40:18 +0300

    Урок 3.23. Список пользователей

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -3,10 +3,29 @@
 namespace app\controllers\admin;
 
 use app\models\admin\User;
+use RedBeanPHP\R;
+use wfm\Pagination;
 
 /** @property User $model */
 
 class UserController extends AppController { // Контроллер для работы с пользовательской части админки
+
+    public function indexAction () { // Метод для работы с пользователями в админ-панели
+
+        // Нам нужно показать список пользователей
+        $page = get ('page'); // Получаем номер текущей страницы
+        $perpage = 10; // Количество записей на одной странице
+        $total = R::count ('user'); // Получаем общее количество пользователей из БД
+        $pagination = new Pagination($page, $perpage, $total); // Получаем объект пагинации. Передаем на вход нужные параметры $page, $perpage, $total
+        $start = $pagination->getStart(); // Переменная $start - с какой позиции нам начинать выбирать записи из БД при выдаче на страницу при пагинации
+
+        $users = $this->model->get_users ($start, $perpage); // Получаем список пользователей из БД используя метод get_users модели User
+
+        $title = 'Список пользователей'; // Объявляем переменную $title
+        $this->setMeta ("Админка :: {$title}"); // Передаем название title в представление
+        $this->set (compact ('title', 'users', 'pagination', 'total')); // Передаем саму переменную в вид
+
+    }
 
     public function loginAdminAction () { // Метод для страницы авторизации администратора
 
--- app/models/admin/User.php
+++ app/models/admin/User.php
@@ -2,11 +2,19 @@
 
 namespace app\models\admin;
 
+use RedBeanPHP\R;
+
 class User extends \app\models\User { // Модель (класс) для работы с админкой, наследует класс User пользовательской части сайта
 
     public static function isAdmin (): bool { // Метод проверки пользователя на роль администратора
 
         return (isset($_SESSION['user']) && $_SESSION['user']['role'] == 'admin'); // Если в массиве $_SESSION по ключу 'user' есть данные И в этом же массиве по ключу 'user' и 'role' == 'admin' - вернем true (авторизованный пользователь админ)
+    }
+
+    public function get_users ($start, $perpage): array { // Метод для получения информации обо всех пользователях из БД
+
+        return R::findAll ("user", "LIMIT $start, $perpage"); // Получим данные всех пользователей. Используем метод findAll для таблицы "user" с лимитом выдачи по параметрам $start, $perpage
+
     }
 
 }
